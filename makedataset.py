48d8b8c999f9ee98aa23cb68bd9f102ff20ade04
stanley-zheng <stanleyjzheng@gmail.com>
2020-07-17 22:43:10 -0600

    Update makedataset.py

--- makedataset.py
+++ makedataset.py
@@ -9,6 +9,7 @@ def importSQL():
 	conn = sqlite3.connect(os.path.sep.join([f'{workingDirectory}', 'input', 'FPA_FOD_20170508.sqlite']))
 	df = pd.read_sql_query("SELECT DISCOVERY_DOY,CONT_DOY,FIRE_SIZE,LATITUDE,LONGITUDE,STATE FROM Fires", con = conn)
 	print(df.head())
+	print
 	return df
 
 def makeTest(df):
@@ -17,17 +18,13 @@ def makeTest(df):
 		content = f.readlines() 
 	content = [int(x.strip()) for x in content] 
 	ver = df.loc[content]
-	train = pd.concat([ver, df])
-	train = df.reset_index(drop=True)
-	train_gpby = df.groupby(list(df.columns))
-	idx = [x[0] for x in train_gpby.groups.values() if len(x) == 1]
-	train = df.reindex(idx)
+	train = pd.concat([ver,df]).drop_duplicates(keep=False)
 	return train, ver
 
 def dataset():
 	train, ver = makeTest(importSQL())
 	return train, ver
 
-dataset()
+train, ver = dataset()
 print(train.shape)
 print(ver.shape)
