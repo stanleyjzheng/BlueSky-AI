2b2d1d60940ee1bac4fa2f1995d2c3d2b4c2903c
stanley-zheng <stanleyjzheng@gmail.com>
2020-07-18 12:37:08 -0600

    Update worldis2.py

--- worldis2.py
+++ worldis2.py
@@ -1,29 +1,19 @@
-# experimental optimizer func:
-#optimizer = keras.optimizers.SGD(learning_rate=0.1, nesterov=True)
-#optimizer = keras.optimizers.Aadam(learning_rate=0.05)
 from makedataset import dataset
-import pandas as pd
-import numpy as np
-from collections import namedtuple
-import torch.nn as nn
-import torch.nn.functional as F
-from torch import Tensor
 import torch
 import numpy
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import metrics
 
-train, trainLabels, verLabels, ver = dataset()
+train, trainLabels, ver, verLabels = dataset()
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(1, 4)),
     keras.layers.Dense(14, activation="relu"),
-    keras.layers.Dense(6, activation='relu'),
+	keras.layers.Dense(9, activation='relu'),
     keras.layers.BatchNormalization(),
     keras.layers.Dense(3, activation="softmax")
     ])
-
 
 model.compile(
     optimizer= 'adam', 
@@ -31,8 +21,4 @@ model.compile(
         metrics=["accuracy"]
 )
 
-model.fit(train, trainLabels, batch_size = 2048, epochs = 5, validation_data = (ver, verLabels))
-
-
-
-
+model.fit(train, trainLabels, batch_size = 2048, epochs = 100, validation_data = (ver, verLabels))
