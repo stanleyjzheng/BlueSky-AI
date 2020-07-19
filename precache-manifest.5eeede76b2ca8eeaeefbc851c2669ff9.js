364e2a5c0adbdf963db73130c43ce509e3525626
stanley-zheng <stanleyjzheng@gmail.com>
2020-07-19 16:19:22 -0600

    Updates

---
+++ precache-manifest.5eeede76b2ca8eeaeefbc851c2669ff9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987a9fcdc9600623d034f6fb9880a463",
+    "url": "/BlueSky-AI/index.html"
+  },
+  {
+    "revision": "84a2438864d06c0d5bd1",
+    "url": "/BlueSky-AI/static/css/2.e9146003.chunk.css"
+  },
+  {
+    "revision": "43f5c8b6ac2509de45f8",
+    "url": "/BlueSky-AI/static/css/main.986d73bc.chunk.css"
+  },
+  {
+    "revision": "84a2438864d06c0d5bd1",
+    "url": "/BlueSky-AI/static/js/2.c3ca5081.chunk.js"
+  },
+  {
+    "revision": "accb96bcda8a0b27e52e071204adf2f0",
+    "url": "/BlueSky-AI/static/js/2.c3ca5081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f5c8b6ac2509de45f8",
+    "url": "/BlueSky-AI/static/js/main.8ce18e98.chunk.js"
+  },
+  {
+    "revision": "c138fcef1e48efb88521",
+    "url": "/BlueSky-AI/static/js/runtime-main.48299029.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BlueSky-AI/static/media/logo.5d5d9eef.svg"
+  }
+]);
